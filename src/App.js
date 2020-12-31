fc6b0e287295fd01bf69d396d8e96e092a4b12fe
cultureweb <cedric.milius@yahoo.fr>
2020-12-31 23:54:16 +0100

    implement and install pusher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import SideBar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("0ab2632253bd2b0896bc", {
+      cluster: "eu"
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", data => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app__body">

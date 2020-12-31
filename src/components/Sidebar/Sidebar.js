0132cbcc0033faa924c26bdc0b44da9c9a89c805
cultureweb <cedric.milius@yahoo.fr>
2020-12-31 16:57:53 +0100

    side bar header with avatar and icons inline styled

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Sidebar.css";
+import {
+  DonutLarge as DonutLargeIcon,
+  Chat as ChatIcon,
+  MoreVert as MoreVertIcon,
+} from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
 
 const Sidebar = () => {
-  return <div className="sidebar">I am the Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://www.e-real.fr/wp-content/themes/perso/images/icone-avatar.png" />
+        {/* <Avatar src="https://avatars2.githubusercontent.com/u/35487627?s=460&u=0f7f49fea6388f0991868e3721887405ccbe8033&v=4" /> */}
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyANgPM7O32ebw7-K0R6H7jc47Kq1b-7qos",
+  authDomain: "whatsapp-client-1872a.firebaseapp.com",
+  projectId: "whatsapp-client-1872a",
+  storageBucket: "whatsapp-client-1872a.appspot.com",
+  messagingSenderId: "544021224811",
+  appId: "1:544021224811:web:2207d33c1517c6d576402e",
+};

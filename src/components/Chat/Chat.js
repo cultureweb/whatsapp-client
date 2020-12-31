dbb0354ab92ae66d1d569cea98198a426f33ada0
cultureweb <cedric.milius@yahoo.fr>
2020-12-31 19:05:38 +0100

    chat body message hardcoded

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import "./Chat.css";
 import {
   AttachFile,
+  Message,
   MoreVert as MoreVertIcon,
   SearchOutlined,
 } from "@material-ui/icons";
@@ -10,25 +11,35 @@ import {
 const Chat = () => {
   return (
     <div className="chat">
-      <div className="chat">
-        <div className="chat__header">
-          <Avatar />
-          <div className="chat__headerInfo">
-            <h3>Room name</h3>
-            <p>Last seen at ...</p>
-          </div>
-          <div className="chat__headerRight">
-            <IconButton>
-              <SearchOutlined />
-            </IconButton>
-            <IconButton>
-              <AttachFile />
-            </IconButton>
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          </div>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
         </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Peter</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Peter</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
       </div>
     </div>
   );

5c7dff923966c119ad1116226fd5aad51be3a297
cultureweb <cedric.milius@yahoo.fr>
2020-12-31 18:22:58 +0100

    chat header hardcoded for now with style

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
 import "./Chat.css";
+import {
+  AttachFile,
+  MoreVert as MoreVertIcon,
+  SearchOutlined,
+} from "@material-ui/icons";
 
 const Chat = () => {
-  return <div className="chat">I am the Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat">
+        <div className="chat__header">
+          <Avatar />
+          <div className="chat__headerInfo">
+            <h3>Room name</h3>
+            <p>Last seen at ...</p>
+          </div>
+          <div className="chat__headerRight">
+            <IconButton>
+              <SearchOutlined />
+            </IconButton>
+            <IconButton>
+              <AttachFile />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;

cb706f945039570e79199095f850277a85e51928
cultureweb <cedric.milius@yahoo.fr>
2021-01-01 15:45:22 +0100

    deployment heroku adress

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 // const header = await createToken();
 
 const instance = axios.create({
-  baseURL: "http://localhost:47001/api/v1",
+  // baseURL: "http://localhost:47001/api/v1",
+  baseURL: "https://whatsapp-server-api.herokuapp.com/api/v1",
   // headers: { Authorization: "Bearer " + token },
 });
 export default instance;
